86ee9881d95290118511d3ed9b305b76713bad49
Muhammad Fiaz <heckias47@gmail.com>
2018-07-31 21:13:41 +0500

    laravel login, signup and ACL

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/', 'PostController@index')->name('home');
+
+Route::resource('users', 'UserController');
+
+Route::resource('roles', 'RoleController');
+
+Route::resource('permissions', 'PermissionController');
+
+Route::resource('posts', 'PostController');
